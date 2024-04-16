ee6d774af8f238698b1fffdacfea047fe22740e4
AsianCawFee <darryllee1999@gmail.com>
2024-04-16 02:27:17 -0500

    Create Cell Prefab

---
+++ Scripts/Prefabs/Cell.gd
@@ -0,0 +1,18 @@
+extends Control
+
+@export var num : int
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	$Label.text = str(num)
+	pass
+
+
+func _on_area_2d_body_entered(body):
+	if(body.is_in_group("PuzzlePiece")):
+		print($"Label".text," Collided")
+	pass # Replace with function body.
