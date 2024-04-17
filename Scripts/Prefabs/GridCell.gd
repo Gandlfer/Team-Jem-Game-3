299518b85975c95d030d0b645b1284a280dd0d8c
AsianCawFee <darryllee1999@gmail.com>
2024-04-17 16:24:28 -0500

    Randomize the blocked grids

--- Scripts/Prefabs/GridCell.gd
+++ Scripts/Prefabs/GridCell.gd
@@ -1,13 +1,23 @@
 extends Node2D
 
 var cell = preload("res://Scenes/Prefabs/Cell.tscn")
-
+var rng = RandomNumberGenerator.new()
+var arr = [-1,-1,-1]
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	var numbers = []
+	for x in range(0,36):
+		numbers.append(x)
+
+	for x in range (0,3):
+		var randNum = rng.randi_range(0,numbers.size()-1)
+		arr[x] = numbers[randNum]
+		numbers.remove_at(randNum)
+			
 	for x in range(0,36):
 		var node = cell.instantiate()
 		node.num=x
-		if x == 7 or x == 21 or x == 30:
+		if x in arr:#x == 7 or x == 21 or x == 30:#
 			node.get_node("GridCellBody/Area2DCell").add_to_group("BlockedBox")
 			node.get_node("GridCellBody/Area2DCell/Blocked").visible = true
 		#print(x)
