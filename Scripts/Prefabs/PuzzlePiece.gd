93e7698c0d1cdaa1abee8a90d6fa06f501a027b1
AsianCawFee <darryllee1999@gmail.com>
2024-04-21 23:08:16 -0500

    Collision with blockedbox and outofbounds

--- Scripts/Prefabs/PuzzlePiece.gd
+++ Scripts/Prefabs/PuzzlePiece.gd
@@ -5,6 +5,8 @@ var outofbound = false
 var blocked = false
 var dropable = false
 var pieces = false
+
+var collidedPiecesList = {}
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -13,7 +15,12 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	#print(!outofbound," ", !blocked ," ", dropable)
-	collided = !outofbound and !blocked and dropable and !pieces
+	if collidedPiecesList.size()>0:
+		collided = false and dropable #and !pieces
+	else:
+		collided = true and dropable #and !pieces
+	#collided = !outofbound and !blocked and dropable and !pieces
+	#print(collidedPiecesList.size())
 	#if collided == true:
 		#print(collided)
 	pass
@@ -24,22 +31,33 @@ func _on_area_2d_body_entered(body):
 	#print(body)
 	#print(get_child(0)==body)
 	if body.is_in_group("PuzzlePiece") and !(get_child(0)==body):
-		print("Something")
+		#print("Something")
+		#print("PuzzlePiece ",body.get_node("../Label").text,body.get_node("../../..").name)
+		#collidedPiecesList[body.get_node("../../..").name] = ""
+		#print(collidedPiecesList)
 		pieces = true
 		
 
 
 func _on_area_2d_body_exited(body):
 	if body.is_in_group("PuzzlePiece") and !(get_child(0)==body):
+		#print("Exit PuzzlePiece ",body.get_node("../Label").text,body.get_node("../../..").name)
+		#collidedPiecesList.erase(body.get_node("../../..").name)
+		#print(collidedPiecesList)
 		pieces = false
 
 
 func _on_area_2d_area_entered(area):
 	if area.is_in_group("OutofBounds"):
+		#print("OutofBounds ",area.name)
+		collidedPiecesList[area.name] = ""
 		outofbound = true
 	if area.is_in_group("BlockedBox"):
+		#print("BlockedBox ",area.get_node("../../Label").text)
+		collidedPiecesList[area.get_node("../../Label").text] = ""
 		blocked = true
 	if area.is_in_group("Dropable"):
+		#print("Dropable ",area.get_node("..").name)
 		dropable = true
 		#print("In")
 
@@ -48,8 +66,10 @@ func _on_area_2d_area_exited(area):
 		#print("Out")
 		dropable = false
 	if area.is_in_group("BlockedBox"):
+		collidedPiecesList.erase(area.get_node("../../Label").text)
 		blocked = false
 	if area.is_in_group("OutofBounds"):
+		collidedPiecesList.erase(area.name)
 		outofbound = false
 		#print("Here")
 
