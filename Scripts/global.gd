54f4cf460faebc3e494c7deb521bcbc8a678c9ef
AsianCawFee <darryllee1999@gmail.com>
2024-04-22 00:21:19 -0500

    Added level variable to create number of blocked box

--- Scripts/global.gd
+++ Scripts/global.gd
@@ -3,7 +3,7 @@ extends Node
 var is_dragging = false
 var needAdd = false
 var node = ""
-
+var level = 3
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
