4cd95a4ba86def1839cd8b6f7fbd147b81db8aa1
CawFee <darryllee1999@gmail.com>
2024-04-24 04:20:10 +0800

    Adding a next scene button when completion

--- Scenes/Puzzle/Sprite2D.gd
+++ Scenes/Puzzle/Sprite2D.gd
@@ -1,4 +1,4 @@
-extends Sprite2D
+extends TextureButton
 
 
 # Called when the node enters the scene tree for the first time.
@@ -10,6 +10,14 @@ func _ready():
 func _process(delta):
 	if Global.achieved:
 		get_node(".").visible = true
+		get_node(".").disabled = false
 	else:
 		get_node(".").visible = false
+		get_node(".").disabled = true
 	pass
+
+
+
+func _on_pressed():
+	print("Next Scene")
+	pass # Replace with function body.
