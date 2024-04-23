e2a717859099ae44a0d54f98a8696f4428dcc433
AsianCawFee <darryllee1999@gmail.com>
2024-04-23 00:10:09 -0500

    Create checking if the requested potion has been achieved

--- Scripts/global.gd
+++ Scripts/global.gd
@@ -1,7 +1,24 @@
 extends Node
 
+var attr = ["Health","Strength","Wisdom","Agility","Dexerity"]
+var requested = {}
+var currentMade = {}
 var is_dragging = false
 var needAdd = false
 var node = ""
-var level = 3
+var level = 5
 var piecesInside = {"ZShape":{},"LShape":{},"TShape":{},"IShape":{},"Square_shape":{}}
+var achieved = true
+func _process(delta):
+	#checkAchieved()
+	achieved = checkAchieved()
+			
+func checkAchieved():
+	var done = true
+	for x in requested.keys():
+		if x not in currentMade.keys():
+			done = done and false
+		else:
+			done = done and true
+			
+	return done
