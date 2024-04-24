712e5a12d8ec34a139c4e41b72e0f95cd4f097ba
AsianCawFee <darryllee1999@gmail.com>
2024-04-24 17:01:00 -0500

    Potion Progress

--- Scripts/Prefabs/Progress Bar.gd
+++ Scripts/Prefabs/Progress Bar.gd
@@ -8,26 +8,30 @@ func _ready():
 	$TShape.rounded = true
 	$IShape.rounded = true
 	$Square_shape.rounded = true
+	$PotionProgress.rounded = true
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	for x in Global.piecesInside.keys():
-		get_node(x).value = Global.piecesInside[x].size() * 33.3
-		var attr 
-		match x:
-			"ZShape":
-				attr = "Health"
-			"LShape":
-				attr = "Wisdom"
-			"TShape":
-				attr = "Agility"
-			"IShape":
-				attr = "Strength"
-			"Square_shape":
-				attr = "Dexerity"
-		if Global.piecesInside[x].size() >= 3:
-			Global.currentMade[attr]= ""
-		else:
-			Global.currentMade.erase(attr)
+		if x != "FillerShape":
+			get_node(x).value = Global.piecesInside[x].size() * 33.3
+			var attr 
+			match x:
+				"ZShape":
+					attr = "Health"
+				"LShape":
+					attr = "Wisdom"
+				"TShape":
+					attr = "Agility"
+				"IShape":
+					attr = "Strength"
+				"Square_shape":
+					attr = "Dexerity"
+			if Global.piecesInside[x].size() >= 3:
+				Global.currentMade[attr]= ""
+			else:
+				Global.currentMade.erase(attr)
+	
+	get_node("PotionProgress").value = (Global.sum / 28.0) *100
