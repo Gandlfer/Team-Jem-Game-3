7db6fe5d6db0b35f65e809dbc6ff22fd71d36783
Kennedy <71536021+CodingKen02@users.noreply.github.com>
2024-04-28 02:11:02 -0500

    Hand Animation (For Mouse Clicking Currently)
    for clicking until controls are set for buttons

--- Scenes/RightHand.gd
+++ Scenes/RightHand.gd
@@ -3,6 +3,10 @@ extends CharacterBody2D
 const MAX_SPEED = 500
 const ACCELERATION = 800
 const FRICTION = 1000
+@onready var hand_sprite = $HoldingHandPngClipart
+var hand_closed = false
+var hand_open_texture = preload("res://Scenes/Hands/rightOpen.png")
+var hand_closed_texture = preload("res://Scenes/Hands/rightClosed.png")
 
 func _physics_process(delta):
 	get_input()
@@ -11,3 +15,8 @@ func _physics_process(delta):
 func get_input():
 	var input_direction = Input.get_vector("move_left_r", "move_right_r", "move_up_r", "move_down_r")
 	velocity = input_direction * MAX_SPEED
+
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		hand_sprite.texture = hand_closed_texture
+	else:
+		hand_sprite.texture = hand_open_texture
