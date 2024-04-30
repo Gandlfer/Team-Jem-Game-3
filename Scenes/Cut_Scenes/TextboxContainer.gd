742d6f98787ee4dc8f093e8fe6d946654177028a
Allison <allisonlyons777@gmail.com>
2024-04-30 15:26:30 -0500

    Text Box text resolved

--- Scenes/Cut_Scenes/TextboxContainer.gd
+++ Scenes/Cut_Scenes/TextboxContainer.gd
@@ -5,8 +5,9 @@ const TEXT_DELAY = 3.0
 
 @onready var textbox_container = $TextboxContainer
 @onready var label = $MarginContainer/HBoxContainer/Label
-var timer := Timer.new()
-signal timeout
+#@onready var timer : Timer = $Timer
+#var timer := Timer.new()
+#signal timeout
 
 enum State {
 	READY,
@@ -21,22 +22,30 @@ var current_text = ""
 var current_index = 0
 
 func _ready():
+	
 	if textbox_container == null or label == null:
 		print("Textbox container or label not found.")
 		hide_textbox()
 		queue_text("Hault Witch! You will not be escaping!.... You make me a potion that I want! But I'm not telling you what potion I want. HAHA ")
 
 func _process(delta):
+	var timer : Timer = Timer.new()
+	add_child(timer)
+	timer.one_shot = true
+	timer.autostart = false
+	timer.wait_time = 3.0
+	timer.timeout.connect(_timer_Timeout)
+	timer.start()
 	# queue_text("Hault Witch! You will not be escaping!.... You make me a potion that I want! But I'm not telling you what potion I want. HAHA ")
 	match current_state:
 		State.READING:
 			if current_index < current_text.length():
 				label.text += current_text[current_index]
 				current_index += 1
-				timer.start(CHAR_READ_RATE)
+				#timer.start(CHAR_READ_RATE)
 			else:
 				current_state = State.PAUSED
-				timer.start(TEXT_DELAY)
+				#timer.start(TEXT_DELAY)
 		State.PAUSED:
 			pass
 		_:
@@ -61,23 +70,8 @@ func display_text():
 		current_index = 0
 		current_state = State.READING
 		show_textbox()
-		timer.start(TEXT_DELAY) # Start the timer to delay displaying the next text
+		#timer.start(TEXT_DELAY) # Start the timer to delay displaying the next text
 	else:
 		current_state = State.FINISHED
-
-func _on_Timer_timeout():
-	match current_state:
-		State.PAUSED:
-			timer.stop()
-			current_state = State.READY
-			if text_queue.size() > 0:
-				display_text()
-		_:
-			if current_index < current_text.length():
-				label.append_text(current_text[current_index])
-				current_index += 1
-			else:
-				timer.stop()
-				current_state = State.READY
-				if text_queue.size() > 0:
-					display_text()
+func _timer_Timeout():
+	queue_free()
