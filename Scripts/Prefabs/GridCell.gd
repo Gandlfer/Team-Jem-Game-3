ef4953dad7019ab34a4c08f9cd4546073b674a1d
AsianCawFee <darryllee1999@gmail.com>
2024-05-03 14:18:56 -0500

    Fix for unsolvable 3rd level

--- Scripts/Prefabs/GridCell.gd
+++ Scripts/Prefabs/GridCell.gd
@@ -7,29 +7,37 @@ var bgMusic : AudioStreamMP3
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	#var temp = []
+	#temp.remove_at()
 	bgMusic = load("res://Audio/027682_boss39s-music-for-gamemp3-68640.mp3")
 	bgMusicPlayer = AudioStreamPlayer.new()  # Create a new AudioStreamPlayer for the background music
 	bgMusic.loop = true  # Set loop to true for the background music
 	bgMusicPlayer.stream = bgMusic
 	bgMusicPlayer.volume_db = -10
 	add_child(bgMusicPlayer)
-	bgMusicPlayer.play()
+	#bgMusicPlayer.play()
 	
 	
-	var numbers = []
+	var numbers = {}
 	var arr = []
 	for x in range(0,36):
-		numbers.append(x)
+		numbers[x]=""
+		#numbers.append(x)
+
 	for x in range (0,Global.level):
-		var randNum = rng.randi_range(0,numbers.size()-1)
-		arr.append(numbers[randNum])
+		#print(len(numbers.keys()))
+		var randNum = rng.randi_range(0,len(numbers.keys())-1)
+		#print(numbers.keys())
+		arr.append(numbers.keys()[randNum])
+		removeAround(numbers.keys()[randNum],numbers)
 		#arr[x] = numbers[randNum]
-		numbers.remove_at(randNum)
+		#numbers.erase(randNum)
+		#numbers.remove_at(randNum)
 			
 	for x in range(0,36):
 		var node = cell.instantiate()
 		node.num=x
-		#node.get_node("Lable").visible = false
+		#node.get_node("Label").visible = false
 		if x in arr:#x == 7 or x == 21 or x == 30:#
 			node.get_node("GridCellBody/Area2DCell").add_to_group("BlockedBox")
 			node.get_node("GridCellBody/Area2DCell/Blocked").visible = true
@@ -40,8 +48,28 @@ func _ready():
 func _process(delta):
 	pass
 
-
+func removeAround(x : int, dict):
+	var positions= [[-7,-6,-5],[-1,0,+1],[5,6,7]]
+	for arr in positions:
+		for y in arr:
+			dict.erase(x+y)
+			
+	#var row
+	#var col
+	#if x/6==0:
+		#row = 0
+	#elif x/6 == 5:
+		#row = 2
+	#if x%6 == 0: #first from the left
+		#col = 0
+		##pass
+	#elif x%6 ==5: #first from the right
+		#col = 3
+		#pass
+	#for x 
+	
 func _on_area_2d_body_entered(body):
 	if body.is_in_group("PuzzlePiece"):
 		print("Entered")
 	pass # Replace with function body.
+
