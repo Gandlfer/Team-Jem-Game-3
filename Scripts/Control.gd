4c92c6a7780490beef71a1e6f927e7a1e611b55e
chloepiper <chloepiper1207@gmail.com>
2024-05-05 02:25:42 -0500

    Menu changes

--- Scripts/Control.gd
+++ Scripts/Control.gd
@@ -12,5 +12,17 @@ func _process(delta):
 	pass
 
 
-func _on_button_pressed():
-	get_tree().change_scene_to_file("res://Scenes/Instructions/instructions.tscn")
+func _on_play_button_pressed():
+	get_tree().change_scene_to_file("res://Scenes/Cut_Scenes/cut_scene_1.tcsn")
+
+
+func _on_story_button_pressed():
+	get_tree().change_scene_to_file("res://Scenes/Cut_Scenes/cut_scene_1.tcsn")
+
+
+func _on_controls_button_pressed():
+	get_tree().change_scene_to_file("res://Scenes/instructions.tscn")
+
+
+func _on_quit_button_pressed():
+	get_tree().quit()
