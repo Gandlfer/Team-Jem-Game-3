c7f7c12d886cf826f456e1b1f81d21db0001cd4c
AsianCawFee <darryllee1999@gmail.com>
2024-05-06 14:49:06 -0500

    Missing push for background music

--- Scripts/Control.gd
+++ Scripts/Control.gd
@@ -3,7 +3,13 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$SoundBG.play()
+	if Mainmenubackground.stream_paused:
+		Mainmenubackground.stream_paused=false
+	else:
+		Mainmenubackground.play_music_level()
+	print("Here")
+	#$SoundBG.play()
+	
 	pass # Replace with function body.
 
 
@@ -14,6 +20,7 @@ func _process(delta):
 
 func _on_play_button_pressed():
 	get_tree().change_scene_to_file("res://Scenes/Cut_Scenes/cut_scene_1.tscn")
+	Mainmenubackground.stop_music()
 	Global.menuPlace = 1
 
 
